6adaa365d3b7a211c297acad235bc369f10e5d4a
Akashms45 <akashms452002@gmail.com>
2025-11-28 00:29:52 +0530

    final

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,14 +1,13 @@
-import logo from '../../assest/logo1.png'
+import logo from "../../assest/logo1.png";
 import "../../App.css";
 
-export const Navbar = ()=>{
-    return (
-        <header className='flex px-3 py-2 gap-3 border-b-4 border-gray-200'>
-            <div className='w-12 h-12'>
-                <img className='w-full h-full' src={logo} alt='logo'/>
-            </div>
-            <h1 className='text-indigo-800 text-4xl font-bold'>NOTE-IF</h1>
-            
-        </header>
-    )
-}
+export const Navbar = () => {
+  return (
+    <header className=" flex px-3 py-2 gap-3 border-b-4 border-gray-200">
+      <div className="w-12 h-12">
+        <img className="w-full h-full" src={logo} alt="logo" />
+      </div>
+      <h1 className="text-indigo-800 text-4xl font-bold">NOTE-IF</h1>
+    </header>
+  );
+};
